ef996d056ba73ed74fcbc3dcd06a088829250234
ValeryVasilevich <>
2024-11-21 14:32:08 +0100

    Init

---
+++ Technical task - lvl1/UserModule/UserViewController.swift
@@ -0,0 +1,12 @@
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
