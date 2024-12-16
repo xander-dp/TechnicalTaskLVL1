2f8f5bc0e1fb39807508dbdffe9adbefd51566a0
xander_savchenko <oleksandr.savchenko@innowise.com>
2024-12-16 11:01:06 +0100

    get rid of unused structures

--- TechnicalTask-lvl1/DataAccessLayer/DALDataModel/DALUser.swift
+++ TechnicalTask-lvl1/DataAccessLayer/DALDataModel/DALUser.swift
@@ -10,11 +10,9 @@ import CoreData
 
 // MARK: - UserEntity
 public struct DALUser: Codable {
-    let id: Int
-    let name, username, email: String
-    let phone, website: String?
+    let name: String
+    let email: String
     let address: Address
-    let company: Company?
     
     func toUserEntity() -> UserEntity {
         UserEntity(email: self.email,
@@ -26,36 +24,6 @@ public struct DALUser: Codable {
 
 // MARK: - Address
 public struct Address: Codable {
-    let street, city: String
-    let suite, zipcode: String?
-    let geo: Geo?
-}
-
-// MARK: - Geo
-public struct Geo: Codable {
-    let lat, lng: String?
-    
-    init?(lat: String?, lng:String?) {
-        if lat == nil || lng == nil {
-            return nil
-        }
-        
-        self.lat = lat
-        self.lng = lng
-    }
-}
-
-// MARK: - Company
-public struct Company: Codable {
-    let name, catchPhrase, bs: String?
-    
-    init?(name: String?, catchPhrase: String?, bs: String?) {
-        if name == nil {
-            return nil
-        }
-        
-        self.name = name
-        self.catchPhrase = catchPhrase
-        self.bs = bs
-    }
+    let street: String
+    let city: String
 }
